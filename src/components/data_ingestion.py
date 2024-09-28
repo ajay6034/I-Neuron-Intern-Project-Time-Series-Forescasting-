e92e0fac922613092a456cbad188160085cab035
AJAY KUMAR JAGU <ajaykumarjagu155@gmail.com>
2024-09-27 23:27:00 -0400

    artifacts

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -19,7 +19,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Enter the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook/data/Updated_tesla_stock_data.csv')
+            df=pd.read_csv('notebook/data/tesla_stock_data.csv')
             logging.info('Exported or read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
